d2b29de802438f166d95564f6ad5cad1dc2b8116
nagcse42 <nagcse42@gmail.com>
2021-06-05 11:13:23 +0400

    getting location using navigator chganges committed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-google-maps';
+
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('Location not enabled for this device..')
+    } else {
+      navigator.geolocation.getCurrentPosition((location) => {
+        console.log(`latitude: ${location.coords.latitude}, logitude: ${location.coords.longitude}`)
+      });
+    }
+  }
+
+  watchPosition() {
+    navigator.geolocation.watchPosition((location) => {
+      console.log(`latitude: ${location.coords.latitude}, logitude: ${location.coords.longitude}`)
+    }, (error) => {
+      console.log(error);
+    }, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
+  }
 }
